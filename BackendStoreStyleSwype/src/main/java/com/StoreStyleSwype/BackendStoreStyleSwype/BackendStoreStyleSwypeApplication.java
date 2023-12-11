81466c0996031ac554f26fb9b3e31715e248c9ef
bilrazor <113417155+bilrazor@users.noreply.github.com>
2023-12-11 13:08:13 +0100

    Creación del proyecto

---
+++ BackendStoreStyleSwype/src/main/java/com/StoreStyleSwype/BackendStoreStyleSwype/BackendStoreStyleSwypeApplication.java
@@ -0,0 +1,13 @@
+package com.StoreStyleSwype.BackendStoreStyleSwype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStoreStyleSwypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStoreStyleSwypeApplication.class, args);
+	}
+
+}
